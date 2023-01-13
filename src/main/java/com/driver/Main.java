d14930959d0d7d7fb10ad73d9e3f2b2a6c038e7c
Saravanan Rajasekharan <saravanan.rajasekharan@gmail.com>
2023-01-13 21:11:37 +0530

    Added files

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args){
+
+        RWOnly read = new RWOnly();
+
+        read.getName();
+        read.setName("private variable access changed");
+        read.getName();
+
+    }
 }
